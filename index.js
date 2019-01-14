12724b4f794acd7de22edbfd0519594a7188c6c6
vijay pratap singh <vijay.singh2@globallogic.com>
2019-01-14 17:40:12 +0530

    gitingnore added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port=process.env.PORT || 7000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello World   VIJAY</h1>');
 });
 
 
